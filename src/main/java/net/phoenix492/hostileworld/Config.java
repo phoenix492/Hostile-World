05c72008472903bca4ee0d39fcb953eefbf18b98
pieman492 <rauljrrauljr@gmail.com>
2025-09-04 01:44:41 -0500

    Fix duplicate lines in config

--- src/main/java/net/phoenix492/hostileworld/Config.java
+++ src/main/java/net/phoenix492/hostileworld/Config.java
@@ -6,19 +6,19 @@ public class Config {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
 
     public static final ModConfigSpec.IntValue FUNGAL_INFECTION_BIOME_BUILDUP = BUILDER
-        .comment(" Amount of infection units per tick to build up on players in biomes tagged with hostileworld:builds_fungal_infection", " Default: 3")
+        .comment(" Amount of infection units per tick to build up on players in biomes tagged with hostileworld:builds_fungal_infection")
         .defineInRange("fungalInfectionBiomeBuildup", 3, 0, Integer.MAX_VALUE);
 
     public static final ModConfigSpec.IntValue FUNGAL_INFECTION_DROPOFF = BUILDER
-        .comment(" Amount of infection units to take away from players each tick.", " Applied even when players are in infectious biomes, so effectively subtracts 1 from the above number.", " Default: 1")
+        .comment(" Amount of infection units to take away from players each tick.", " Applied even when players are in infectious biomes, so effectively subtracts 1 from the above number.")
         .defineInRange("fungalInfectionDropoff", 1, 0, Integer.MAX_VALUE);
 
     public static final ModConfigSpec.IntValue FUNGAL_INFECTION_MINIMUM = BUILDER
-        .comment(" Minimum value for infection buildup on a player.", " Default: 0")
+        .comment(" Minimum value for infection buildup on a player.")
         .defineInRange("fungalInfectionMinimum", 0, 0, Integer.MAX_VALUE);
 
     public static final ModConfigSpec.IntValue FUNGAL_INFECTION_MAXIMUM = BUILDER
-        .comment(" Maximum value for infection buildup on a player.", " Default: " + Integer.MAX_VALUE)
+        .comment(" Maximum value for infection buildup on a player.")
         .defineInRange("fungalInfectionMaximum", Integer.MAX_VALUE, 0, Integer.MAX_VALUE);
     static final ModConfigSpec SPEC = BUILDER.build();
 
