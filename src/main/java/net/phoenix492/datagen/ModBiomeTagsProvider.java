a86da2778509685de79f593143e54e32b797b557
pieman492 <rauljrrauljr@gmail.com>
2025-09-04 00:35:55 -0500

    Tag Mushroom fields as only infectious biome

--- src/main/java/net/phoenix492/datagen/ModBiomeTagsProvider.java
+++ src/main/java/net/phoenix492/datagen/ModBiomeTagsProvider.java
@@ -3,8 +3,10 @@ package net.phoenix492.datagen;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.BiomeTagsProvider;
+import net.minecraft.world.level.biome.Biomes;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.phoenix492.hostileworld.HostileWorld;
+import net.phoenix492.util.TagKeys;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.concurrent.CompletableFuture;
@@ -12,7 +14,8 @@ import java.util.concurrent.CompletableFuture;
 public class ModBiomeTagsProvider extends BiomeTagsProvider {
     @Override
     protected void addTags(HolderLookup.Provider provider) {
-
+        tag(TagKeys.Biomes.FUNGAL_INFECTION)
+            .add(Biomes.MUSHROOM_FIELDS);
     }
 
     public ModBiomeTagsProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, @Nullable ExistingFileHelper existingFileHelper) {
