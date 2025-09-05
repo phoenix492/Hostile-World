2e8521f323a10f08571786a5ab74809a220cc50a
pieman492 <rauljrrauljr@gmail.com>
2025-09-05 14:10:17 -0500

    Make Mycelium also spread to grass.

---
+++ src/main/java/net/phoenix492/mixin/SpreadingSnowyDirtBlockInvoker.java
@@ -0,0 +1,21 @@
+package net.phoenix492.mixin;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.SpreadingSnowyDirtBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(SpreadingSnowyDirtBlock.class)
+public interface SpreadingSnowyDirtBlockInvoker {
+    @Invoker("canPropagate")
+    static boolean invokeCanPropagate(BlockState state, LevelReader levelReader, BlockPos pos) {
+        throw new AssertionError();
+    }
+
+    @Invoker("canBeGrass")
+    static boolean invokeCanBeGrass(BlockState state, LevelReader levelReader, BlockPos pos) {
+       throw new AssertionError();
+    }
+}
