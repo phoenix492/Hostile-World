8f1ef8adedf813d7d8f796610ef01a77ac15fc7a
pieman492 <rauljrrauljr@gmail.com>
2025-09-05 03:41:22 -0500

    Fix config mistake

--- src/main/java/net/phoenix492/hostileworld/Config.java
+++ src/main/java/net/phoenix492/hostileworld/Config.java
@@ -5,6 +5,7 @@ import net.neoforged.neoforge.common.ModConfigSpec;
 public class Config {
     private static final ModConfigSpec.Builder SERVER_BUILDER = new ModConfigSpec.Builder();
 
+    // TODO: Move this from a config to a datamap
     public static final ModConfigSpec.IntValue FUNGAL_INFECTION_BIOME_BUILDUP = SERVER_BUILDER
         .comment(" Amount of infection units per tick to build up on players in biomes tagged with hostileworld:builds_fungal_infection")
         .defineInRange("fungalInfectionBiomeBuildup", 3, 0, Integer.MAX_VALUE);
@@ -20,7 +21,6 @@ public class Config {
     public static final ModConfigSpec.IntValue FUNGAL_INFECTION_MAXIMUM = SERVER_BUILDER
         .comment(" Maximum value for infection buildup on a player.")
         .defineInRange("fungalInfectionMaximum", Integer.MAX_VALUE, 0, Integer.MAX_VALUE);
-    static final ModConfigSpec SERVER_SPEC = SERVER_BUILDER.build();
 
     public static final ModConfigSpec.IntValue FUNGICIDE_REDUCTION_PER_LEVEL = SERVER_BUILDER
         .comment(" Amount of infection buildup reduced per level of Fungicide effect applied.")
@@ -30,4 +30,6 @@ public class Config {
         .comment(" Amount of damage dealt per level of Fungicide effect applied.")
         .defineInRange("fungicideDamagePerLevel", 2.0, 0.0, Double.MAX_VALUE);
 
+    // This goes last. Don't forget.
+    static final ModConfigSpec SERVER_SPEC = SERVER_BUILDER.build();
 }
