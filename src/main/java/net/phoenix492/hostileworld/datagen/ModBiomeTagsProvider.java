dc560d181ea4008db83859370d8805e0161aa196
pieman492 <rauljrrauljr@gmail.com>
2026-02-21 16:38:31 -0600

    Tag biomes

--- src/main/java/net/phoenix492/hostileworld/datagen/ModBiomeTagsProvider.java
+++ src/main/java/net/phoenix492/hostileworld/datagen/ModBiomeTagsProvider.java
@@ -7,6 +7,8 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.BiomeTagsProvider;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BiomeTags;
+import net.neoforged.neoforge.common.Tags;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 
 import java.util.concurrent.CompletableFuture;
@@ -17,10 +19,40 @@ public class ModBiomeTagsProvider extends BiomeTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider provider) {
+
+        // Mod Tags
         tag(ModTagKeys.Biomes.BUILDS_FUNGAL_INFECTION_SLOW)
             .addOptional(ResourceLocation.fromNamespaceAndPath("minecraft", "dark_forest"));
         tag(ModTagKeys.Biomes.BUILDS_FUNGAL_INFECTION_STANDARD)
             .addOptional(ResourceLocation.fromNamespaceAndPath("minecraft", "mushroom_fields"));
+        tag(ModTagKeys.Biomes.BUILDS_FUNGAL_INFECTION_FAST)
+            .addOptional(ResourceLocation.fromNamespaceAndPath(HostileWorld.MODID, "shroom_caves"))
+            .addOptional(ResourceLocation.fromNamespaceAndPath(HostileWorld.MODID, "shroom_core"));
+
+        // C Tags
+        tag(Tags.Biomes.IS_HOT)
+            .addOptional(ResourceLocation.fromNamespaceAndPath(HostileWorld.MODID, "shroom_caves"))
+            .addOptional(ResourceLocation.fromNamespaceAndPath(HostileWorld.MODID, "shroom_core"));
+        tag(Tags.Biomes.IS_HOT_OVERWORLD)
+            .addOptional(ResourceLocation.fromNamespaceAndPath(HostileWorld.MODID, "shroom_caves"))
+            .addOptional(ResourceLocation.fromNamespaceAndPath(HostileWorld.MODID, "shroom_core"));
+        tag(Tags.Biomes.IS_MUSHROOM)
+            .addOptional(ResourceLocation.fromNamespaceAndPath(HostileWorld.MODID, "shroom_caves"))
+            .addOptional(ResourceLocation.fromNamespaceAndPath(HostileWorld.MODID, "shroom_core"));
+        tag(Tags.Biomes.IS_UNDERGROUND)
+            .addOptional(ResourceLocation.fromNamespaceAndPath(HostileWorld.MODID, "shroom_caves"))
+            .addOptional(ResourceLocation.fromNamespaceAndPath(HostileWorld.MODID, "shroom_core"));
+        tag(Tags.Biomes.IS_CAVE)
+            .addOptional(ResourceLocation.fromNamespaceAndPath(HostileWorld.MODID, "shroom_caves"))
+            .addOptional(ResourceLocation.fromNamespaceAndPath(HostileWorld.MODID, "shroom_core"));
+        tag(Tags.Biomes.IS_RARE)
+            .addOptional(ResourceLocation.fromNamespaceAndPath(HostileWorld.MODID, "shroom_caves"))
+            .addOptional(ResourceLocation.fromNamespaceAndPath(HostileWorld.MODID, "shroom_core"));
+
+        // MC Tags
+        tag(BiomeTags.IS_OVERWORLD)
+            .addOptional(ResourceLocation.fromNamespaceAndPath(HostileWorld.MODID, "shroom_caves"))
+            .addOptional(ResourceLocation.fromNamespaceAndPath(HostileWorld.MODID, "shroom_core"));
     }
 
     public ModBiomeTagsProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, @Nullable ExistingFileHelper existingFileHelper) {
